ff29befa82d3d058d46874984d3a4ad63afa291e
Monicaritz16 <161623933+Monicaritz16@users.noreply.github.com>
2024-07-24 11:10:29 -0700

    Initial Commit

---
+++ EnsembleOMDB/EnsembleOMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnsembleOMDBApp.swift
+//  EnsembleOMDB
+//
+//  Created by Monica on 2024-07-24.
+//
+
+import SwiftUI
+
+@main
+struct EnsembleOMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
